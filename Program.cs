7bd0d72df18af73bb67e9538a3f952ce5debb1b9
Maqsudkhan <maqsudtorayev2003@gmail.com>
2023-04-01 15:44:45 +0500

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -6,3 +6,10 @@ System.Console.WriteLine("Converting...");
 int age = Convert.ToInt32(ageString);
 System.Console.WriteLine($"Successfully converted! {age}");
 int kescha = 3;
+
+System.Console.WriteLine($"You are  older than KESCHA: {age > kescha}");
+System.Console.WriteLine($"You are  older than or equal to KESCHA: {age >= kescha}");
+System.Console.WriteLine($"You are younger than KESCHA: {age < kescha}");
+System.Console.WriteLine($"You are younger than or equal to KESCHA: {age <=kescha}");
+System.Console.WriteLine($"Your age  is equal to KESCHA's age: {age == kescha}");
+System.Console.WriteLine($"Your age is not equal to KESCHA's age: {age != kescha}");
