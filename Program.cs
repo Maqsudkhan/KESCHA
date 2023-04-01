873d5ba384097d2e558ac55f7373d39b1246e72e
Maqsudkhan <maqsudtorayev2003@gmail.com>
2023-04-01 15:39:41 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+System.Console.Write("Enter you age: ");
+string ageString = Console.ReadLine();
+System.Console.WriteLine("Converting...");
+int age = Convert.ToInt32(ageString);
+System.Console.WriteLine($"Successfully converted! {age}");
+int kescha = 3;
